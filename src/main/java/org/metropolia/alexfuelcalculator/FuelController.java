4f8a8acc294c55a1511743cfb5c74688654c0140
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-27 14:23:14 +0200

    App works. Yey

--- src/main/java/org/metropolia/alexfuelcalculator/FuelController.java
+++ src/main/java/org/metropolia/alexfuelcalculator/FuelController.java
@@ -1,14 +1,37 @@
 package org.metropolia.alexfuelcalculator;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FuelController {
     @FXML
-    private Label welcomeText;
-
+    private TextField distanceTextField, fuelTextField;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button buttonCalculate;
+    @FXML
+    private Label labelResult;
+    public void initialize(){
+        buttonCalculate.setOnAction(event -> {
+            try
+            {
+                double distance = Double.parseDouble(distanceTextField.getText());
+                double fuel = Double.parseDouble(fuelTextField.getText());
+                if(distance < 0 || fuel < 0)
+                {
+                    labelResult.setText("Result: N/A" );
+                }
+                labelResult.setText("Result: " + String.format("%.2f", fuel/distance * 100) + "l/100km");
+            }catch (NumberFormatException e)
+            {
+                labelResult.setText("Result: N/A" );
+            }
+            finally {
+                distanceTextField.clear();
+                fuelTextField.clear();
+            }
+        });
+
     }
 }
