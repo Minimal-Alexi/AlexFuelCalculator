82d9d6b11c6679c8aba55038750a726aedec3c28
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-27 13:53:45 +0200

    Initial start.

--- src/main/java/org/metropolia/alexfuelcalculator/FuelController.java
+++ src/main/java/org/metropolia/alexfuelcalculator/FuelController.java
@@ -3,7 +3,7 @@ package org.metropolia.alexfuelcalculator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class FuelController {
     @FXML
     private Label welcomeText;
 
